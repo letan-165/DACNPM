3a91b53f1bb6b1b2e6f647de919078cef23ab7a9
letan-165 <2251160057@ut.edu.vn>
2025-08-26 16:00:45 +0700

    init backend

---
+++ backend/src/main/java/com/app/mevocab/MevocabApplication.java
@@ -0,0 +1,13 @@
+package com.app.mevocab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MevocabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MevocabApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/app/mevocab/MevocabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.mevocab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MevocabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
