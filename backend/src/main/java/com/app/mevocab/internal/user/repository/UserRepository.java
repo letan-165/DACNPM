fc807357791673a963962f050871e0a3a407a40e
letan-165 <2251160057@ut.edu.vn>
2025-09-03 13:04:57 +0700

    refactor: edit userRepo username

--- backend/src/main/java/com/app/mevocab/internal/user/repository/UserRepository.java
+++ backend/src/main/java/com/app/mevocab/internal/user/repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends MongoRepository<User,String> {
     Optional<User> findByUsername(String name);
-    boolean existsByName(String name);
+    boolean existsByUsername(String name);
 }
